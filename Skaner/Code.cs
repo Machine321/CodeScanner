4e84c62ee29962ba9b05e44f7ce1bffa9bb69bd0
mateusz.pastok <mateusz.pastok@gmail.com>
2020-11-19 15:47:34 +0100

    Collecting data in collection and view in form. Reacting on Enter key.

---
+++ Skaner/Code.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skaner
+{
+    class Code : INotifyPropertyChanged
+    {
+        private String codeString;
+        private DateTime date;
+        public String CodeString
+        {
+            get
+            {
+                return codeString;
+            }
+            set
+            {
+                this.codeString = value;
+                this.OnPropertyChanged("CodeString");
+            }
+        }
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                this.date = value;
+                this.OnPropertyChanged("Date");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- Skaner/MainWindow.xaml.cs
+++ Skaner/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,49 @@ namespace Skaner
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+        ObservableCollection<Code> codesList;
+
         public MainWindow()
         {
+            codesList = new ObservableCollection<Code>();
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CodesDataGrid.DataContext = codesList;
+            CodeTextBox.Focus();
+        }
+
+        private void CodeTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter || e.Key != Key.Return) return;
+
+            e.Handled = true;
+            codesList.Add(new Code {
+                CodeString = CodeTextBox.Text,
+                Date = DateTime.Now.Date
+            });
+            CodeTextBox.Text = "";
+        }
+
+        private void CodeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //codesList.Add(new Code
+            //{
+            //    CodeString = CodeTextBox.Text,
+            //    Date = DateTime.Now.Date
+            //});
+            //CodeTextBox.Text = "";
+            Console.WriteLine(codesList.Count);
         }
     }
 }
